cc85fd8f4c74affa7d626ce11a1a6f770f7468bc
AngelRysu <jesus.quecam@gmail.com>
2025-02-25 12:56:05 -0600

    Se modifica database para el funcionamiento correcto del test y git actions

--- src/models/config/database.ts
+++ src/models/config/database.ts
@@ -4,30 +4,27 @@ import { setupModels } from '../types';
 
 dotenv.config();
 
-let sequelize;
-
-if (process.env.NODE_ENV === 'test') {
-  sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: ':memory:',
-    logging: false,
-    define: {
-      timestamps: false,
-    },
-  });
-} else {
-  sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    logging: process.env.NODE_ENV !== 'development',
-    define: {
-      timestamps: false,
-    },
-  });
-}
+const sequelize: Sequelize =
+  process.env.NODE_ENV === 'test'
+    ? new Sequelize({
+        dialect: 'sqlite',
+        storage: ':memory:',
+        logging: false,
+        define: {
+          timestamps: false,
+        },
+      })
+    : new Sequelize({
+        dialect: 'mysql',
+        host: process.env.DB_HOST as string,
+        username: process.env.DB_USER as string,
+        password: process.env.DB_PASSWORD as string,
+        database: process.env.DB_NAME as string,
+        logging: process.env.NODE_ENV !== 'development',
+        define: {
+          timestamps: false,
+        },
+      });
 
 setupModels(sequelize);
 sequelize.sync({ force: false });
