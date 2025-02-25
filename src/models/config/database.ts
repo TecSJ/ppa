3139fc9da3081a23065c3df78d4dad062258eb6b
AngelRysu <jesus.quecam@gmail.com>
2025-02-25 12:48:05 -0600

    se implementa base de datos para pruebas

--- src/models/config/database.ts
+++ src/models/config/database.ts
@@ -1,22 +1,35 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import { setupModels } from '../types'; // Importa el setup de modelos
+import { setupModels } from '../types';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: process.env.NODE_ENV !== 'development',
-  define: {
-    timestamps: false,
-  },
-});
+let sequelize;
+
+if (process.env.NODE_ENV === 'test') {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: ':memory:',
+    logging: false,
+    define: {
+      timestamps: false,
+    },
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    logging: process.env.NODE_ENV !== 'development',
+    define: {
+      timestamps: false,
+    },
+  });
+}
 
 setupModels(sequelize);
-sequelize.sync({ force: false })
+sequelize.sync({ force: false });
 
 export default sequelize;
