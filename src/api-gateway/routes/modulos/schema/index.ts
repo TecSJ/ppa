d70a5aef2defd01aeaa14f03303ffa47ede5ba3f
AngelRysu <jesus.quecam@gmail.com>
2025-03-05 13:31:26 -0600

    fix test

--- src/api-gateway/routes/modulos/schema/index.ts
+++ src/api-gateway/routes/modulos/schema/index.ts
@@ -1,4 +1,4 @@
 export { createModuloSchema } from './create.modulo.schema'
 export { findAllModulosSchema } from './findAll.modulo.schema';
-export { findOneModuloSchema } from './findOne.MODULO.schema';
+export { findOneModuloSchema } from './findOne.modulo.schema';
 export { updateModuloSchema } from './update.modulo.schema';
