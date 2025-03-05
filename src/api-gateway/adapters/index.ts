deabc11500926ae1d52e9f9cef54eb9bb47b64af
AngelRysu <jesus.quecam@gmail.com>
2025-03-05 13:52:24 -0600

    Se agregan cambios

--- src/api-gateway/adapters/index.ts
+++ src/api-gateway/adapters/index.ts
@@ -1,5 +1,9 @@
 export { actividadHandlers } from './actividad/handlers';
 export { asignaturaHandlers } from './asignatura/handlers';
 export { competenciaHandler } from './competencia/handlers';
+<<<<<<< HEAD
 export { ofertaHandlers } from './oferta/handlers';
 export { planHandlers } from './planes/handlers';
+=======
+export { moduloHandler } from './modulo/handlers';
+>>>>>>> ef91cc0 (Se crea handlers modulo)
