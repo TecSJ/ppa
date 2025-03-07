283ef286deddc42336916c94bdb68ddd84bea150
AngelRysu <jesus.quecam@gmail.com>
2025-03-07 16:50:55 -0600

    creacion de otro archivo

--- src/api-gateway/routes/actividades/schema/index.ts
+++ src/api-gateway/routes/actividades/schema/index.ts
@@ -1,7 +1,5 @@
 export { createActividadSchema } from './create.actividad.schema';
 export { deleteActividadSchema } from './delete.actividad.schema';
 export { findAllActividadesSchema } from './findAll.actividad.schema';
-import { findOneActividadesSchema } from './findOne.actividad.schema';
 export { updateActividadesSchema } from './update.actividad.schema';
-
-export {findOneActividadesSchema};
+export { findOneActividadesSchema } from './findOn.actividad.schema';
