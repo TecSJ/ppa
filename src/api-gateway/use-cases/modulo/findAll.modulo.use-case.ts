2f1c59f3a262b31922c4fa0ad93af903cdbd2a73
AngelRysu <jesus.quecam@gmail.com>
2025-03-13 11:58:36 -0600

    Se modifica el use case find all para obtener los datos requeridos

--- src/api-gateway/use-cases/modulo/findAll.modulo.use-case.ts
+++ src/api-gateway/use-cases/modulo/findAll.modulo.use-case.ts
@@ -1,12 +1,49 @@
 import { findAllQuery } from 'models/queries';
 import { Modulo } from 'models/types/Modulos';
+import { Planes } from 'models/types/Planes';
+import { Programa } from 'models/types/Programas';
 
 type FindAllModulosData = Partial<Modulo>;
 
 const findAllModulos = async (where?: FindAllModulosData) => {
-  return await findAllQuery(Modulo)({
-    ...where,
+  const modulos = await findAllQuery(Modulo)(
+    where,
+    [
+      {
+        model: Planes,
+        as: "plan",
+        attributes: ["clave", "idPrograma"],
+        include: [
+          {
+            model: Programa,
+            as: "programa",
+            attributes: ["abreviatura", "idPlantel"],
+          },
+        ],
+      },
+    ]
+  );
+  const formattedModulos = modulos.map((modulo) => {
+    const moduloJSON = modulo.toJSON();
+    const plan = moduloJSON.plan || {};
+    const programa = plan.programa || {};
+
+    return {
+      idModulo: moduloJSON.idModulo,
+      idPlan: moduloJSON.idPlan,
+      clave: moduloJSON.clave,
+      abreviatura: moduloJSON.abreviatura,
+      nombre: moduloJSON.nombre,
+      creditos: moduloJSON.creditos,
+      asignaturas: moduloJSON.asignaturas,
+      tipo: moduloJSON.tipo,
+      estado: moduloJSON.estado,
+      planDeEstudio: plan.clave,
+      carrera: programa.abreviatura,
+      unidadAcademica: programa.idPlantel,
+    };
   });
+  return formattedModulos;
 };
 
 export { findAllModulos };
