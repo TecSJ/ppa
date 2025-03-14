3ac4100f7c62eeacf418d565da51fc75e7e86a32
AngelRysu <jesus.quecam@gmail.com>
2025-03-14 10:05:30 -0600

    Se modifica el archivo de barril y modelo Modulo

--- src/models/types/index.ts
+++ src/models/types/index.ts
@@ -1,16 +1,14 @@
-import { Sequelize } from 'sequelize';
-import { Actividad, ActividadesSchema } from './Actividades';
-import { Asignatura, AsignaturasSchema } from './Asignaturas';
-import { Competencia, CompetenciasSchema } from './Competencias';
-import { Conocimiento, ConocimientosSchema } from './Conocimientos';
-import { Programa, ProgramasSchema } from './Programas';
-import { Modulo, ModulosSchema } from './Modulos'
-import { Planes, Planeschema } from './Planes';
-import { Oferta, OfertasSchema } from './Ofertas';
-
+import { Sequelize, Model } from "sequelize";
+import { Actividad, ActividadesSchema } from "./Actividades";
+import { Asignatura, AsignaturasSchema } from "./Asignaturas";
+import { Competencia, CompetenciasSchema } from "./Competencias";
+import { Conocimiento, ConocimientosSchema } from "./Conocimientos";
+import { Programa, ProgramasSchema } from "./Programas";
+import { Modulo, ModulosSchema } from "./Modulos";
+import { Planes, Planeschema } from "./Planes";
+import { Oferta, OfertasSchema } from "./Ofertas";
 
 export function setupModels(sequelize: Sequelize): void {
-    // Aquí se definen los modelos
     Actividad.init(ActividadesSchema, Actividad.config(sequelize));
     Asignatura.init(AsignaturasSchema, Asignatura.config(sequelize));
     Competencia.init(CompetenciasSchema, Competencia.config(sequelize));
@@ -19,11 +17,10 @@ export function setupModels(sequelize: Sequelize): void {
     Planes.init(Planeschema, Planes.config(sequelize));
     Modulo.init(ModulosSchema, Modulo.config(sequelize));
     Oferta.init(OfertasSchema, Oferta.config(sequelize));
-    
-    // Aquí se definen las relaciones
-    Asignatura.associate(sequelize.models);
-    Conocimiento.associate(sequelize.models);
-    Modulo.associate(sequelize.models);
-    Oferta.associate(sequelize.models);
-    Planes.associate(sequelize.models);
+
+    Object.values(sequelize.models).forEach((model: typeof Model) => {
+        if ('associate' in model) {
+            (model as typeof Model & { associate: (models: typeof sequelize.models) => void }).associate(sequelize.models);
+        }
+    });
 }
