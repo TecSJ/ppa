87fcbbab2c1e170e73f5f7a321ff7c68aabbec05
AngelRysu <jesus.quecam@gmail.com>
2025-03-19 12:40:51 -0600

    Se modifica find all de modulo (Use case)

--- src/api-gateway/routes/modulos/index.ts
+++ src/api-gateway/routes/modulos/index.ts
@@ -29,7 +29,7 @@ function Router(fastify: FastifyInstance): void{
     );
 
     fastify.get(
-        '/fa',
+        '/',
         {
             schema:{
                 ...findAllModulosSchema,
